cb755f4389309f64adccc63403fff4af5b906fd1
thế anh <theanh682001@gmail.com>
2022-08-21 21:46:05 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/youtubletest/MainActivity.java
+++ app/src/main/java/com/example/youtubletest/MainActivity.java
@@ -71,7 +71,7 @@ public class MainActivity extends  YouTubeBaseActivity  {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode==1)
         {
-            Toast.makeText(this, "Lỗi Rồi", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Lỗi Rồi sao t ko chuyển đc qua android", Toast.LENGTH_SHORT).show();
         }
     }
     public void getPlaylist(String url,Callback callback)
